0c6c86885804814f607d85a4bab7b2179fd8f7f3
ZhouWenxing <64086566+zhouwenxing666@users.noreply.github.com>
2025-08-15 00:37:54 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,6 @@ Core model definitions including conceptual encoders and Taylor networks
 """
 
 from .batm_concept_encoder import ConceptNet, ExU
-from .batm_taylor_network import Fast_Tucker_Taylor
+from .batm_taylor_network import BATM_Fast_Tucker_Taylor
 
-__all__ = ['ConceptNet', 'ExU', 'Fast_Tucker_Taylor']
+__all__ = ['ConceptNet', 'ExU', 'BATM_Fast_Tucker_Taylor']
